76ba886acf77c926613a190efbadf3ee8d389e51
khiromoshito <labanan02@gmail.com>
2021-05-25 01:24:46 +0800

    Main: added stuff

--- bin/schemas/Camera.js
+++ bin/schemas/Camera.js
@@ -3,8 +3,8 @@ import { ViewStreamer } from "./ViewStreamer.js";
 
 export class Camera {
     /**
-     * Captures views from a `Canvas3D` and streams it to an HTML canvas
-     * @param {{canvas: HTMLCanvasElement, x: number, y: number, z: number, rx: number, y: number, z: number, scale: number}} options
+     * Captures views from a `Canvas3D`
+     * @param {{canvas: HTMLCanvasElement | HTMLCanvasElement[], x: number, y: number, z: number, rx: number, y: number, z: number, scale: number}} options
      */
     constructor(options = {}) {
 
@@ -12,6 +12,14 @@ export class Camera {
          * @type {ViewStreamer}
         */
         this.streamer = new ViewStreamer();
+
+        if("canvas" in options) {
+            if(options.canvas instanceof Array)
+                for(let canvas of options.canvas)
+                    this.streamer.addCanvas(canvas);
+            else this.streamer.addCanvas(options.canvas); 
+        }
+            
     }
 
     
